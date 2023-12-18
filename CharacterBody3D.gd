d55152eab3901ec2209ec2616048c8ca51eba3b4
Magifire <65313702+Magifire@users.noreply.github.com>
2023-12-18 00:37:07 -0500

    First Major
    Set up character body, added chicken as model, fixed character movement, added skybox, added debug texture and floor in debug level, changed debug level size, I should probably make multiple commits next time im stupid lol

--- CharacterBody3D.gd
+++ CharacterBody3D.gd
@@ -1,12 +1,14 @@
 extends CharacterBody3D
 
 
-const SPEED = 5.0
-const JUMP_VELOCITY = 4.5
+const SPEED = 7.5
+const JUMP_VELOCITY = 9
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
 
+func _ready():
+	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
 
 func _physics_process(delta):
 	# Add the gravity.
@@ -29,3 +31,21 @@ func _physics_process(delta):
 		velocity.z = move_toward(velocity.z, 0, SPEED)
 
 	move_and_slide()
+
+var mouse_sens = 0.01
+
+func _input(event):
+	if event.is_action_pressed("ui_cancel"):
+		Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
+	
+	if event.is_action_pressed("Shoot"):
+		if Input.mouse_mode == Input.MOUSE_MODE_VISIBLE:
+			Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+			#get_tree().set_input_as_handled() use if needed, atm Idk what it does tbh
+		
+	if event is InputEventMouseMotion and Input.mouse_mode == Input.MOUSE_MODE_CAPTURED:
+		rotate_y(-event.relative.x * mouse_sens)
+		$Camera3D.rotate_x(-event.relative.y * mouse_sens)
+		$Camera3D.rotation.x = clampf($Camera3D.rotation.x, -deg_to_rad(90), deg_to_rad(90))
+
+
